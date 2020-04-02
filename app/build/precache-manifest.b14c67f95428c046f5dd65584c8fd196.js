124ee6ccf35792a78e2410bcea9162b53964aa37
Felix <felix@xingren.com>
2020-04-02 23:40:41 +0800

    publish: v0.0.1

---
+++ app/build/precache-manifest.b14c67f95428c046f5dd65584c8fd196.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa90788ca0cee885f571a86c5dee618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64aa54c3644c3187a868",
+    "url": "/static/css/main.87da4e28.chunk.css"
+  },
+  {
+    "revision": "228712ef875beece7158",
+    "url": "/static/js/2.ef2a7dda.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.ef2a7dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64aa54c3644c3187a868",
+    "url": "/static/js/main.6014a7e0.chunk.js"
+  },
+  {
+    "revision": "2fde43049dd024e3e925",
+    "url": "/static/js/runtime-main.fac7c3c4.js"
+  },
+  {
+    "revision": "47bce5c74f589f4867dbd57e9ca9f808",
+    "url": "/static/media/README.47bce5c7.md"
+  },
+  {
+    "revision": "ab6161aa2116e818a896d6ee25af9fcb",
+    "url": "/static/media/README.ab6161aa.md"
+  },
+  {
+    "revision": "fc35fdc70d5fc69d269883a822c7a53e",
+    "url": "/static/media/README.fc35fdc7.md"
+  }
+]);
