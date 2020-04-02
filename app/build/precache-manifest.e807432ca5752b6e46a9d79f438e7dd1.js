bbf1cbecd3ce54ca7076bd77c3a2ec2d0a8d30d7
Felix <felix@xingren.com>
2020-04-03 01:00:47 +0800

    publish: 发布 v0.0.3

---
+++ app/build/precache-manifest.e807432ca5752b6e46a9d79f438e7dd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b3b33bd26267d32b5d1c4996aa82e7",
+    "url": "/frontend-docs/index.html"
+  },
+  {
+    "revision": "f4ba9b974ebbb239c354",
+    "url": "/frontend-docs/static/css/main.87da4e28.chunk.css"
+  },
+  {
+    "revision": "228712ef875beece7158",
+    "url": "/frontend-docs/static/js/2.ef2a7dda.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/frontend-docs/static/js/2.ef2a7dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ba9b974ebbb239c354",
+    "url": "/frontend-docs/static/js/main.bc630c03.chunk.js"
+  },
+  {
+    "revision": "4a3b55de1bb7a2a3aa63",
+    "url": "/frontend-docs/static/js/runtime-main.0871066d.js"
+  },
+  {
+    "revision": "47bce5c74f589f4867dbd57e9ca9f808",
+    "url": "/frontend-docs/static/media/README.47bce5c7.md"
+  },
+  {
+    "revision": "ab6161aa2116e818a896d6ee25af9fcb",
+    "url": "/frontend-docs/static/media/README.ab6161aa.md"
+  },
+  {
+    "revision": "fc35fdc70d5fc69d269883a822c7a53e",
+    "url": "/frontend-docs/static/media/README.fc35fdc7.md"
+  }
+]);
--- app/build/service-worker.js
+++ app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c28854f150c8f7b525a7ccad155a173.js"
+  "/frontend-docs/precache-manifest.e807432ca5752b6e46a9d79f438e7dd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend-docs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
